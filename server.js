229fad807ef7dcdd78eb57df7716b0b427ecb19e
Kaaedex <55245295+Kaaedex@users.noreply.github.com>
2022-08-10 21:31:34 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = request('express');
+const app = express();
+
+app.get('/', (req, res) => {res.send('Hello User');});
+app.get('/sgIndex', async (req, res) => {res.send("Scrap Guilds Item Collection");})
+app.get('/sgIndex/:id', async (req, res) => {
+const id = req.params.id;
+try {
+	const sgItem = await getSGItem(id);
+	res.json(sgItem);
+} catch (error) 
+	{
+		console.error(err);
+		res.status(500).json({err: "Something went horribly wrong!"});
+	}
+});
+const port = 8080;
+
+app.listen(port, () => {
+console.log('listening on port port')
+})
