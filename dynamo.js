5092fccdf1d50115b2e9caf60165e339d59ac62e
Kaaedex <55245295+Kaaedex@users.noreply.github.com>
2022-08-10 22:22:21 -0400

    update dynamo.js file

---
+++ dynamo.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+region: process.env.AWS_DEFAULT_REGION,
+accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+
+const getSGItem = async (id) => 
+{
+const params = 
+	{
+	TableName: "TableName",
+	Key: {id}
+	}
+	const sgItem = await dynamoClient.scan(params).promise();
+	return sgItem;
+};
+
+module.exports = 
+{
+getSGItem
+}
